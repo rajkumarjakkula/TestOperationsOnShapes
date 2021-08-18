81b04679fcd97eabd016b4ead9c4eec205c57cc0
rajkumarjakkula <rajkumarjakkula969@gmail.com>
2021-08-18 21:50:02 +0530

    Requirement 1:Finding Area of Rectangle

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,11 @@
 public class Rectangle {
+    int length;
+    int width;
+    public Rectangle(int length,int width){
+        this.length=length;
+        this.width=width;
+    }
+    public int Area() {
+        return length*width;
+    }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RectangleTest {
+    @Test
+    void toCalcuateAreaOfRectangleWithPositiveDimensions(){
+        Rectangle rectangle=new Rectangle(4,2);
+        int actualArea=rectangle.Area();
+        int expectedArea=8;
+        assertEquals(actualArea,expectedArea);
+    }
+
+    @Test
+    void toCalcuateAreaOfRectangleWithNegativeDimensions(){
+        Rectangle rectangle=new Rectangle(-4,-2);
+        int actualArea=rectangle.Area();
+        int expectedArea=8;
+        assertEquals(actualArea,expectedArea);
+    }
+
+    @Test
+    void toCalcuateAreaOfRectangleWithPositiveLengthAndNegativeWidth(){
+        Rectangle rectangle=new Rectangle(4,-2);
+        int actualArea=rectangle.Area();
+        int expectedArea=-8;
+        assertEquals(actualArea,expectedArea);
+
+    }
+
+    @Test
+    void toCalcuateAreaOfRectangleWithNegativeLengthAndPositiveWidth(){
+        Rectangle rectangle=new Rectangle(-4,2);
+        int actualArea=rectangle.Area();
+        int expectedArea=-8;
+        assertEquals(actualArea,expectedArea);
+    }
+
 }
